513883c0c12f318461d683e77201ce9729b46310
deedax <andre.stana@epitech.eu>
2024-02-17 16:59:51 +0100

    [FIX] Fixng the unistallation of configuration

--- unconfig-term.sh
+++ unconfig-term.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Déterminer le gestionnaire de paquets
+if [ -x "$(command -v dnf)" ]; then
+    PKG_MANAGER="dnf"
+    REMOVE_COMMAND="remove"
+elif [ -x "$(command -v apt-get)" ]; then
+    PKG_MANAGER="apt-get"
+    REMOVE_COMMAND="remove"
+elif [ -x "$(command -v pacman)" ]; then
+    PKG_MANAGER="pacman"
+    REMOVE_COMMAND="-Rns"
+else
+    echo "Votre gestionnaire de paquets n'est pas supporté. Vous devez désinstaller manuellement : git, zsh et wget."
+    exit 1
+fi
+
 # Supprimer les plugins zsh
 rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -13,5 +28,15 @@ rm -rf ~/.oh-my-zsh
 # Supprimer le fichier .zshrc
 rm -f ~/.zshrc
 
-# Changer le shell par défaut en bash
-chsh -s $(which bash)
+# Désinstaller les paquets
+sudo $PKG_MANAGER $REMOVE_COMMAND -y git zsh wget
+
+# Changer le shell par défaut à l'ancien shell
+if [ -f ~/.default_shell ]; then
+    chsh -s $(cat ~/.default_shell)
+    rm ~/.default_shell
+else
+    echo "Impossible de rétablir le shell par défaut, le fichier ~/.default_shell n'existe pas."
+fi
+
+echo $SHELL > ~/.bash
