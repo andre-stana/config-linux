bb63cd90fad85b6a7aaa23f3e82280eef6d8cd85
deedax <andre.stana@epitech.eu>
2024-02-17 17:15:40 +0100

    [FIX] Fixng the config problem

--- config-term-dnf.sh
+++ config-term-dnf.sh
@@ -3,17 +3,23 @@
 # Déterminer le gestionnaire de paquets
 if [ -x "$(command -v dnf)" ]; then
     PKG_MANAGER="dnf"
+    INSTALL_CMD="sudo dnf install -y"
+    VS_CODE_PKG="https://code.visualstudio.com/sha/download?build=stable&os=linux-rpm-x64"
 elif [ -x "$(command -v apt-get)" ]; then
     PKG_MANAGER="apt-get"
+    INSTALL_CMD="sudo apt-get install -y"
+    VS_CODE_PKG="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 elif [ -x "$(command -v pacman)" ]; then
     PKG_MANAGER="pacman -S"
+    INSTALL_CMD="sudo pacman -S --noconfirm"
+    VS_CODE_PKG="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 else
-    echo "Votre gestionnaire de paquets n'est pas supporté. Vous devez installer manuellement : git, zsh, wget et emacs."
+    echo "Votre gestionnaire de paquets n'est pas supporté. Vous devez installer manuellement : git, zsh, wget, emacs et vscode."
     exit 1
 fi
 
 # Installer les paquets nécessaires
-sudo $PKG_MANAGER install -y git zsh wget emacs
+$INSTALL_CMD git zsh wget emacs
 
 # Télécharger et installer oh-my-zsh
 RUNZSH=no sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
@@ -40,6 +46,10 @@ ZSH_THEME="themelol"
 plugins=(git zsh-autosuggestions zsh-syntax-highlighting web-search macos docker git screen xcode encode64 z sudo vscode)
 source $ZSH/oh-my-zsh.sh
 EOF
+
+# Télécharger et installer Visual Studio Code
+wget -O vscode.deb $VS_CODE_PKG
+$INSTALL_CMD ./vscode.deb
 
 # Remplacer le shell actuel par zsh
 exec zsh -l
